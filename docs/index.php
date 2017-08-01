f4520ac2ccfd02c4056a5f6b4952d346cbf1d03e
Peter Maselkowski <pmaselkowski@gmail.com>
2017-08-01 15:20:09 +0200

    * docs

--- docs/index.php
+++ docs/index.php
@@ -1,16 +1,23 @@
 <?php require __DIR__ . '/_header.php'; ?>
+<!-- trim -->
 <title>Documentation</title>
 <h1>Documentation</h1>
+<!-- /trim -->
 <?php
 $textLines = [
-	'These are demos for various binding handlers.<br />',
-	'This text for example is editable, and is automatically updated in javascript view model.<br />',
-	'Hit ctrl + <b>b</b>, <i>i</i>, <u>u</u> for <b>bold</b>, <i>italic</i>, <u>underline</u>, or try your other known formatting shortcuts, try drag and drop, paste.<br />',
+	'These are demos and documentation for various binding handlers.<br />',
+	'This text for example is editable, and is automatically',
+	'updated in JavaScript view model.<br />',
+	'Hit ctrl + <b>b</b>, <i>i</i>, <u>u</u> for <b>bold</b>,',
+	'<i>italic</i>, <u>underline</u>, or try your other known ',
+	'formatting shortcuts, try drag and drop, paste.<br />',
 	'You can read or modify this text via JavaScript:<br />',
 	'<ol>',
 	'<li>Open a console</li>',
-	'<li>Type `balin.model.intro.text` and press enter. Then change something and try again.</li>',
-	'<li>This text should appear also below second time, but without editing mode.</li>',
+	'<li>Type <code>balin.model.intro.text</code> and press enter. Then ',
+	'change something and try again.</li>',
+	'<li>This text should appear also below second time, ',
+	'but without editing mode. However any changes will be updated instantly</li>',
 	'</ol>'
 ];
 $text = implode("\n\t\t", $textLines);
@@ -20,20 +27,19 @@ array_walk($textLines, function(&$value, $index)
 });
 ?>
 <div data-bind="with: balin.model.intro">
-	<div data-bind="htmlValue: text">
-		<!--For web spiders, this will be replaced by JavaScript, so here can be placed anything, usually empty-->
-		<?= $text; ?>..
-		<!--This comments will vanish too-->
-	</div>
+	<div data-bind="htmlValue: text"></div>
 	<hr />
 	<div data-bind="html: text"></div>
 </div>
 <script type="text/javascript">
 	window.onload = function () {
-		balin.model.intro = new Maslosoft.Ko.BalinDev.Models.Intro({
-			text: [
+		// trim
+		var introText = [
 <?= implode(",\n\t\t\t\t", $textLines) . "\n"; ?>
 			].join("\n")
+		// /trim
+		balin.model.intro = new Maslosoft.Ko.BalinDev.Models.Intro({
+			text: introText
 		});
 		ko.applyBindings({model: balin.model}, document.getElementById('ko-balin'));
 	};
