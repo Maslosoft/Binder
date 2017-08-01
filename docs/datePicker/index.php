57b84b3e68b8a600db356ec15d797c83546a44e9
Peter Maselkowski <pmaselkowski@gmail.com>
2017-08-01 14:29:01 +0200

    * docs

--- docs/datePicker/index.php
+++ docs/datePicker/index.php
@@ -11,11 +11,12 @@ use Maslosoft\Ilmatar\Widgets\JavaScript\Packages\BootstrapDatepickerPackage;
 <title>Date Picker</title>
 <h1>Date Picker</h1>
 <p>
-	Bootstrap datepicker based. Picker is meant to be used additionally, leaving input
+	This binding handler is bootstrap datepicker based. Picker is meant to be used additionally, leaving input
 	for typing with keyboard.
 </p>
+<h4>Ninja Skills</h4>
 <p>
-	This component is enchanced with Date JS, so it has some ninja
+	This component is enchanced with <a href="http://www.datejs.com/" target="_blank">Date JS</a>, so it has some ninja
 	features. Try to type "now", "sunday", "next monday" into date inputs and then press <kbd>Enter</kbd>.
 	Other options include, but are not limited to:
 	<ul>
@@ -25,6 +26,7 @@ use Maslosoft\Ilmatar\Widgets\JavaScript\Packages\BootstrapDatepickerPackage;
 		<li>+2 y</li>
 	</ul>
 </p>
+<h4>Live Example</h4>
 <!-- /trim -->
 <form class="form">
 	<div class="form-group">
@@ -48,12 +50,15 @@ use Maslosoft\Ilmatar\Widgets\JavaScript\Packages\BootstrapDatepickerPackage;
 		</div>
 	</div>
 </form>
-<div data-bind="dateFormatter: balin.model.datePicker.date">
-
-</div>
-<a href="#" data-bind="click: function(){balin.model.datePicker.date = Math.round(Date.now() / 1000) - 200000;}">Set a bit to past</a> <br/>
-<a href="#" data-bind="click: function(){balin.model.datePicker.date = Math.round(Date.now() / 1000);}">Set date to now</a> <br />
-<a href="#" data-bind="click: function(){balin.model.datePicker.date = Math.round(Date.now() / 1000) + 200000;}">Set a bit to future</a>
+<div data-bind="dateFormatter: balin.model.datePicker.date" class="label label-success"></div>
+<!-- trim -->
+<p>
+<br />
+<a class="btn btn-success" href="#" data-bind="click: function(){balin.model.datePicker.date = Math.round(Date.now() / 1000) - 200000;}">Set a bit to past</a>
+<a class="btn btn-success" href="#" data-bind="click: function(){balin.model.datePicker.date = Math.round(Date.now() / 1000);}">Set date to now</a>
+<a class="btn btn-success" href="#" data-bind="click: function(){balin.model.datePicker.date = Math.round(Date.now() / 1000) + 200000;}">Set a bit to future</a>
+</p>
+<!-- /trim -->
 <script>
 	window.onload = (function(){
 		balin.model.datePicker = new Maslosoft.Ko.BalinDev.Models.DatePicker;
